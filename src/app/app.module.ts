d18d541c0df5615d3a7a7a62f9f8a9f88d32a38b
Luiz Bonini <lc.bonini13@gmail.com>
2022-08-12 22:36:35 -0300

    Feat: feito o scss do app e o submit event bind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { NovaTranferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTranferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
